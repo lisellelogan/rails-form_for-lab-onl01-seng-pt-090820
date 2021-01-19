a31e39c2b656312e9bcab8ea49858c02404f6d3c
lisellelogan <liselle.logan@yahoo.com>
2021-01-19 15:11:41 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+james = Student.create(first_name: "James", last_name: "Orlanda")
+shannon = Student.create(first_name: "Shannon", last_name: "Person")
+
+science = SchoolClass.create(title: "Science", room_number: 1)
+math = SchoolClass.create(title: "Math", room_number: 2)
